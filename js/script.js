45766015b09e3c35d9f2f7e9ac75516c078c927e
vanyadr <vanyadr@yandex.ru>
2025-07-02 18:38:11 +0300

    new/add adaptive for header and make mobile burger menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 import {moveBubble} from './modules/headerBubble.js';
+import {toggleAccordion} from './modules/toggleAccordion.js';
+import {toggleBurgerMenu} from './modules/toggleBurger.js';
 
 window.addEventListener('load', () => {
     moveBubble();
+    toggleAccordion();
+    toggleBurgerMenu();
 });
