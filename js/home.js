524ef5f9b35ae58424c751be91dda11067a32803
vanyadr <vanyadr@yandex.ru>
2025-07-14 15:36:31 +0300

    new/add product page

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@
 
 import {controlModel} from './modules/modelRotation.js';
 import { createBorderSvg } from './modules/cardBorder.js';
-import { updateSlider } from './modules/initSlider.js';
+import { updateSlider } from './modules/initHomeSlider.js';
 
 window.addEventListener('load', () => {
     controlModel();
