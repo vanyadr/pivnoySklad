1dd6ec969562362d506a927b0df25c7e41d1bd01
ivan drugov <vanyadr@yandex.ru>
2025-07-25 14:23:06 +0300

    feature/add swiping slider

--- js/modules/handleSlider.js
+++ js/modules/handleSlider.js
@@ -11,6 +11,10 @@ export class HandleSlider {
         this.slidesGap = parseFloat(window.getComputedStyle(this.sliderFlex).columnGap);
         this.offset = 0;
         this.slideIndex = 0;
+        this.maxOffset = 0;
+        this.swipeXStart = 0;
+        this.swipeXEnd = 0;
+        this.swipeXAction = 0;
         this.slideWidth = this.calcSlideWidth();
         this.init();
     }
@@ -23,9 +27,10 @@ export class HandleSlider {
 
         this.sliderFlex.style.cssText = `
             width: ${flexWidth}px;
-            transform: translateX(0);
+            transform: translate3d(0, 0, 0);
             transition: 0.3s all ease;
         `;
+        this.maxOffset = (this.slideWidth + this.slidesGap) * (this.slidesQty - this.slidesPerScreen);
     }
     moveSlider() {
         this.prev.addEventListener('click', () => {
@@ -34,26 +39,62 @@ export class HandleSlider {
         this.next.addEventListener('click', () => {
             this.slidePlus();
         });
+        this.sliderWindow.addEventListener('touchstart', e => this.swipeStart(e));
+        this.sliderWindow.addEventListener('touchmove', e => this.swipeAction(e));
+        this.sliderWindow.addEventListener('touchend', e => this.swipeEnd(e));
+    }
+    render() {
+        this.sliderFlex.style.transform = `translate3d(-${this.offset}px, 0, 0)`;
     }
     slidePlus() {
         if (this.slideIndex === this.slidesQty - this.slidesPerScreen) {
             this.slideIndex = 0;
             this.offset = 0;
         } else {
-            this.slideIndex++;
+            this.slideIndex += this.slidesToSlide;
             this.offset = (this.slideWidth + this.slidesGap) * this.slideIndex;
         };
 
-        this.sliderFlex.style.transform = `translateX(-${this.offset}px)`;
+        this.render();
     }
     slideMinus() {
         if (this.slideIndex === 0) {
             this.slideIndex = this.slidesQty - this.slidesPerScreen;
         } else {
-            this.slideIndex--;
+            this.slideIndex -= this.slidesToSlide;
         };
         this.offset = (this.slideWidth + this.slidesGap) * this.slideIndex;
 
-        this.sliderFlex.style.transform = `translateX(-${this.offset}px)`;
+        this.render();
+    }
+    swipeStart(e) {
+        this.swipeXStart = e.changedTouches[0].clientX;
+        this.swipeXAction = e.changedTouches[0].clientX;
+    }
+    swipeAction(e) {
+        this.offset += (this.swipeXAction - e.changedTouches[0].clientX);
+        this.swipeXAction = e.changedTouches[0].clientX;
+
+        if( this.offset < 0) this.offset = 0;
+        if( this.offset > this.maxOffset) this.offset = this.maxOffset;
+
+        this.render();
+    }
+    swipeEnd(e) {
+        this.swipeXEnd = e.changedTouches[0].clientX;
+        
+        this.slideIndex += Math.round((this.swipeXStart - this.swipeXEnd) / (this.slideWidth + this.slidesGap));
+
+        if (this.slideIndex > this.slidesQty - this.slidesPerScreen) {
+            this.slideIndex = this.slidesQty - this.slidesPerScreen;
+            this.offset = (this.slideWidth + this.slidesGap) * this.slideIndex;
+        } else if (this.slideIndex < 0) {
+            this.slideIndex = 0;
+            this.offset = 0;
+        } else {
+            this.offset = (this.slideWidth + this.slidesGap) * this.slideIndex;
+        }
+
+        this.render();
     }
 }
