058dce146969327cae6b920216397a9bf6c1d80a
Ivan Drugov <vanyadr@yandex.ru>
2025-11-25 14:43:32 +0300

    fix: filters

--- js/modules/openFilters.js
+++ js/modules/openFilters.js
@@ -1,190 +1,202 @@
 export const toggleFiltersItem = function () {
-    const items = document.querySelectorAll('.filters__item-title');
-    const overlay = document.querySelector('.filters-overlay');
-    
-    items.forEach(item => {
-        item.addEventListener('click', function() {
-            if (document.documentElement.clientWidth > 1024) {
-                items.forEach(item => {
-                    if (item.parentNode.classList.contains('selected') && item != this) {
-                        item.parentNode.classList.remove('selected');
-                        item.parentNode.parentNode.querySelector('.filters__menu').classList.remove('opened');
-                    };
-                });
-                if (!overlay.classList.contains('active')) overlay.classList.add('active');
-                this.parentNode.classList.toggle('selected');
-                this.parentNode.parentNode.querySelector('.filters__menu').classList.toggle('opened');
-            }
-        });
-    });
-    
-    overlay.addEventListener('click', () => {
-        if (document.documentElement.clientWidth > 1024) {
-            items.forEach(item => {
-                if (item.parentNode.classList.contains('selected') && item != this) {
-                    item.parentNode.classList.remove('selected');
-                    item.parentNode.parentNode.querySelector('.filters__menu').classList.remove('opened');
-                };
+   const items = document.querySelectorAll(".filters__item-title");
+   const overlay = document.querySelector(".filters-overlay");
+
+   items.forEach((item) => {
+      item.addEventListener("click", function () {
+         if (document.documentElement.clientWidth > 1024) {
+            items.forEach((item) => {
+               if (item.parentNode.classList.contains("selected") && item != this) {
+                  item.parentNode.classList.remove("selected");
+                  item.parentNode.parentNode.querySelector(".filters__menu").classList.remove("opened");
+               }
             });
-            overlay.classList.remove('active');
-        }
-    });
+            if (!overlay.classList.contains("active")) overlay.classList.add("active");
+            this.parentNode.classList.toggle("selected");
+            this.parentNode.parentNode.querySelector(".filters__menu").classList.toggle("opened");
+         }
+      });
+   });
+
+   overlay.addEventListener("click", () => {
+      if (document.documentElement.clientWidth > 1024) {
+         items.forEach((item) => {
+            if (item.parentNode.classList.contains("selected") && item != this) {
+               item.parentNode.classList.remove("selected");
+               item.parentNode.parentNode.querySelector(".filters__menu").classList.remove("opened");
+            }
+         });
+         overlay.classList.remove("active");
+      }
+   });
 };
 
 export const tickMenuItem = function () {
-    const items = document.querySelectorAll('.filters__menu-item');
-    let selectedItems = [];
+   const items = document.querySelectorAll(".filters__menu-item");
+   const overlay = document.querySelector(".filters-overlay");
 
-    function findParentByClassName(element, className) {
-        let elem = element;
+   let selectedItems = [];
 
-        while(!elem.classList.contains(className)) {
-            if(elem.tagName.toLowerCase() == 'html') return false;
-            elem = elem.parentNode;
-        }
+   function findParentByClassName(element, className) {
+      let elem = element;
 
-        return elem;
-    };
+      while (!elem.classList.contains(className)) {
+         if (elem.tagName.toLowerCase() == "html") return false;
+         elem = elem.parentNode;
+      }
 
-    function createBubble(clickedItem) {
-        const bubble = document.createElement('span');
+      return elem;
+   }
 
-        bubble.classList.add('filters__item-select');
-        bubble.dataset.menuId = clickedItem.dataset.menuId;
-        bubble.textContent = clickedItem.textContent;
+   function createBubble(clickedItem) {
+      const bubble = document.createElement("span");
 
-        bubble.addEventListener('click', () => {
-            selectedItems.forEach(selectedItem => {
-                if (selectedItem['menuId'] == bubble.dataset.menuId) {
-                    selectedItem['menuElement'].classList.remove('selected');
-                    const selectedId = selectedItems.indexOf(selectedItem);
-                    selectedItems.splice(selectedId, 1);
-                };
-                bubble.remove();
-            });
-        });
+      bubble.classList.add("filters__item-select");
+      bubble.dataset.menuId = clickedItem.dataset.menuId;
+      bubble.textContent = clickedItem.textContent;
 
-        return bubble;
-    };
+      bubble.addEventListener("click", () => {
+         selectedItems.forEach((selectedItem) => {
+            if (selectedItem["menuId"] == bubble.dataset.menuId) {
+               selectedItem["menuElement"].classList.remove("selected");
+               const selectedId = selectedItems.indexOf(selectedItem);
+               selectedItems.splice(selectedId, 1);
+            }
+            bubble.remove();
+         });
+      });
 
-    let counter = 1;
-    items.forEach(item => {
-        item.dataset.menuId = counter++;
-        item.addEventListener('click', function() {
-            const clickedItem = this;
-            const clickedItemHolder = findParentByClassName(clickedItem, 'filters__item-holder');
-            let selectedInSameMenu = false;
-            let isSame = false;
-            let selectedId = 0;
+      return bubble;
+   }
 
-            selectedItems.forEach(selectedItem => {
-                if (clickedItem.parentNode == selectedItem['menuElement'].parentNode) {
-                    selectedInSameMenu = true;
-                    selectedId = selectedItems.indexOf(selectedItem);
-                };
-                if (clickedItem == selectedItem['menuElement']) isSame = true;
-            });
+   let counter = 1;
+   items.forEach((item) => {
+      item.dataset.menuId = counter++;
+      item.addEventListener("click", function () {
+         const clickedItem = this;
+         const clickedItemHolder = findParentByClassName(clickedItem, "filters__item-holder");
+         let selectedInSameMenu = false;
+         let isSame = false;
+         let selectedId = 0;
 
-            if (!selectedInSameMenu) {
-                clickedItem.classList.add('selected');
-                clickedItemHolder.querySelector('.filters__item-select-block').appendChild(createBubble(clickedItem));
-                const clickedObject = {
-                    menuElement: clickedItem,
-                    menuId: clickedItem.dataset.menuId
-                };
-                selectedItems.push(clickedObject);
-            } else {
-                selectedItems[selectedId]['menuElement'].classList.remove('selected');
-                document.querySelector(`.filters__item-select[data-menu-id='${selectedItems[selectedId]['menuId']}']`).remove();
-                selectedItems.splice(selectedId, 1);
+         selectedItems.forEach((selectedItem) => {
+            if (clickedItem.parentNode == selectedItem["menuElement"].parentNode) {
+               selectedInSameMenu = true;
+               selectedId = selectedItems.indexOf(selectedItem);
+            }
+            if (clickedItem == selectedItem["menuElement"]) isSame = true;
+         });
 
-                if (!isSame) {
-                    clickedItem.classList.add('selected');
-                    clickedItemHolder.querySelector('.filters__item-select-block').appendChild(createBubble(clickedItem));
-                    const clickedObject = {
-                        menuElement: clickedItem,
-                        menuId: clickedItem.dataset.menuId
-                    };
-                    selectedItems.push(clickedObject);
-                };
+         if (!selectedInSameMenu) {
+            clickedItem.classList.add("selected");
+            clickedItemHolder.querySelector(".filters__item-select-block").appendChild(createBubble(clickedItem));
+            const clickedObject = {
+               menuElement: clickedItem,
+               menuId: clickedItem.dataset.menuId,
             };
-        });
-    });
+            selectedItems.push(clickedObject);
+         } else {
+            selectedItems[selectedId]["menuElement"].classList.remove("selected");
+            document
+               .querySelector(`.filters__item-select[data-menu-id='${selectedItems[selectedId]["menuId"]}']`)
+               .remove();
+            selectedItems.splice(selectedId, 1);
+
+            if (!isSame) {
+               clickedItem.classList.add("selected");
+               clickedItemHolder.querySelector(".filters__item-select-block").appendChild(createBubble(clickedItem));
+               const clickedObject = {
+                  menuElement: clickedItem,
+                  menuId: clickedItem.dataset.menuId,
+               };
+               selectedItems.push(clickedObject);
+            }
+         }
+
+         clickedItemHolder.querySelector(".filters__item").classList.remove("selected");
+         clickedItemHolder.querySelector(".filters__menu").classList.remove("opened");
+         overlay.classList.remove("active");
+      });
+   });
 };
 
 export const toggleFiltersBlock = function () {
-    const btn = document.querySelector('.filters__open-btn');
-    const menu = document.querySelector('.filters__holder');
-    const closer = document.querySelector('.filters__closer');
+   const btn = document.querySelector(".filters__open-btn");
+   const menu = document.querySelector(".filters__holder");
+   const closer = document.querySelector(".filters__closer");
 
-    if (btn) {
-        btn.addEventListener('click', () => {
-            if (document.documentElement.clientWidth <= 1024) {
-                menu.classList.add('opened');
-                btn.classList.add('hide');
-                document.querySelector('body').classList.add('filters-opened');
-            }
-        });
-        closer.addEventListener('click', () => {
-            if (document.documentElement.clientWidth <= 1024) {
-                menu.classList.remove('opened');
-                btn.classList.remove('hide');
-                document.querySelector('body').classList.remove('filters-opened');
-            }
-        });
+   if (btn) {
+      btn.addEventListener("click", () => {
+         if (document.documentElement.clientWidth <= 1024) {
+            menu.classList.add("opened");
+            btn.classList.add("hide");
+            document.querySelector("body").classList.add("filters-opened");
+         }
+      });
+      closer.addEventListener("click", () => {
+         if (document.documentElement.clientWidth <= 1024) {
+            menu.classList.remove("opened");
+            btn.classList.remove("hide");
+            document.querySelector("body").classList.remove("filters-opened");
+         }
+      });
 
-        window.addEventListener('resize', () => {
-            if (document.documentElement.clientWidth > 1024) {
-                if(menu.classList.contains('opened')) menu.classList.remove('opened');
-                if (btn.classList.contains('hide')) btn.classList.remove('hide');
-                if (document.querySelector('body').classList.contains('filters-opened')) document.querySelector('body').classList.remove('filters-opened');
-            }
-        });
-    }
+      window.addEventListener("resize", () => {
+         if (document.documentElement.clientWidth > 1024) {
+            if (menu.classList.contains("opened")) menu.classList.remove("opened");
+            if (btn.classList.contains("hide")) btn.classList.remove("hide");
+            if (document.querySelector("body").classList.contains("filters-opened"))
+               document.querySelector("body").classList.remove("filters-opened");
+         }
+      });
+   }
 };
 
 export const toggleFiltersAccordion = function () {
-    const items = document.querySelectorAll('.filters__item-holder');
-    let counter = 1;
-    
-    if (items) {
-        items.forEach(function (item) {
-            const btn = item.querySelector('.filters__item');
-            
-            item.dataset.accordionFilters = counter;
-            btn.dataset.accordionFilters = counter;
-            btn.addEventListener('click', function () {
-                if (document.documentElement.clientWidth <= 1024) {
-                    const correctItem = document.querySelector(`.filters__item-holder[data-accordion-filters="${this.dataset.accordionFilters}"]`);
-                    const correctContent = correctItem.querySelector('.filters__menu');
-                    const correctHeader = correctItem.querySelector('.filters__item');
-                    const contentHeight = correctContent.scrollHeight;
-                    
-                    correctContent.classList.toggle('selected');
+   const items = document.querySelectorAll(".filters__item-holder");
+   let counter = 1;
 
-                    if (correctContent.classList.contains('selected')) {
-                        correctContent.style.height = `${contentHeight}px`;
-                    } else {
-                        correctContent.style.height = 0;
-                    }
+   if (items) {
+      items.forEach(function (item) {
+         const btn = item.querySelector(".filters__item");
 
-                    correctHeader.classList.toggle('selected');
-                }
-            });
-            counter++;
-        });
+         item.dataset.accordionFilters = counter;
+         btn.dataset.accordionFilters = counter;
+         btn.addEventListener("click", function () {
+            if (document.documentElement.clientWidth <= 1024) {
+               const correctItem = document.querySelector(
+                  `.filters__item-holder[data-accordion-filters="${this.dataset.accordionFilters}"]`
+               );
+               const correctContent = correctItem.querySelector(".filters__menu");
+               const correctHeader = correctItem.querySelector(".filters__item");
+               const contentHeight = correctContent.scrollHeight;
 
-        window.addEventListener('resize', () => {
-            if (document.documentElement.clientWidth > 1024) {
-                items.forEach(item => {
-                    const content = item.querySelector('.filters__menu');
-                    if (content.classList.contains('selected')) {
-                        content.classList.remove('selected');
-                        content.style.height = 'auto';
-                    };
-                    if (item.querySelector('.filters__item').classList.contains('selected')) item.querySelector('.filters__item').classList.remove('selected');
-                });
+               correctContent.classList.toggle("selected");
+
+               if (correctContent.classList.contains("selected")) {
+                  correctContent.style.height = `${contentHeight}px`;
+               } else {
+                  correctContent.style.height = 0;
+               }
+
+               correctHeader.classList.toggle("selected");
             }
-        });
-    }
+         });
+         counter++;
+      });
+
+      window.addEventListener("resize", () => {
+         if (document.documentElement.clientWidth > 1024) {
+            items.forEach((item) => {
+               const content = item.querySelector(".filters__menu");
+               if (content.classList.contains("selected")) {
+                  content.classList.remove("selected");
+                  content.style.height = "auto";
+               }
+               if (item.querySelector(".filters__item").classList.contains("selected"))
+                  item.querySelector(".filters__item").classList.remove("selected");
+            });
+         }
+      });
+   }
 };
